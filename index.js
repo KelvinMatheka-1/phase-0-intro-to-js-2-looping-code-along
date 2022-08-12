1d243ba6b9f7571b6c7db3ae66d9d103eeb80914
Kelvin M Matheka <beingmatheka@gmail.com>
2022-08-12 22:02:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event){
+  const messages = []
+  for (let i = 0; i < names.length; i++){
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+
+function countDown(){
+  let countDown = 11;
+  while (countDown > 0){
+    countDown--;
+    console.log(countDown);
+}
+}
